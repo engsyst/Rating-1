c385bdb0dfcfe4ed9776de35330bbe29227abac0
Roman Dashkivskyy <roman_dashkivskyy@epam.com>
2017-02-05 07:06:27 +0200

    Fixed sql script.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,20 +22,19 @@ CREATE TABLE IF NOT EXISTS user
   Email      VARCHAR(255)                                                         NOT NULL UNIQUE,
   Login      VARCHAR(255)                                                         NOT NULL UNIQUE,
   Password   VARCHAR(255)                                                         NOT NULL,
-  last_name  VARCHAR(255)                                                          NOT NULL,
   EmployeeId INT(11)                                                              NOT NULL,
   RoleId     INT(11)                                                              NOT NULL,
   FOREIGN KEY (RoleId) REFERENCES role (RoleId),
   FOREIGN KEY (EmployeeId) REFERENCES employee (EmployeeId)
 );
 
--- INSERT IGNORE INTO role (Title)
--- VALUES ('SUPER_ADMIN');
--- SET @admin_role = LAST_INSERT_ID();
---
--- INSERT IGNORE INTO employee (AcademicTitle, Degree, JobTitle, Name, Patronymic, Surname)
--- VALUES ('Stub', 'Stub', 'Stub', 'Roman', 'Boris', 'Dashkovskyy');
--- SET @admin_employee = LAST_INSERT_ID();
---
--- INSERT IGNORE INTO user (`CreateTime`, `Email`, `Login`, `Password`, `EmployeeId`, `RoleId`)
--- VALUES ('2017-02-05 05:40:19', 'mineholst@gmail.com', 'admin', '$2a$12$0ywBYfgYOf3AjzgXlGiyZuD2mnSRfFF8YMt/NpDO5MqiJZyZEMHDy', @admin_employee, @admin_role);
+INSERT IGNORE INTO role (Title)
+VALUES ('SUPER_ADMIN');
+SET @admin_role = LAST_INSERT_ID();
+
+INSERT IGNORE INTO employee (AcademicTitle, Degree, JobTitle, Name, Patronymic, Surname)
+VALUES ('Stub', 'Stub', 'Stub', 'Roman', 'Boris', 'Dashkovskyy');
+SET @admin_employee = LAST_INSERT_ID();
+
+INSERT IGNORE INTO user (`CreateTime`, `Email`, `Login`, `Password`, `EmployeeId`, `RoleId`)
+VALUES ('2017-02-05 05:40:19', 'mineholst@gmail.com', 'admin', '$2a$12$0ywBYfgYOf3AjzgXlGiyZuD2mnSRfFF8YMt/NpDO5MqiJZyZEMHDy', @admin_employee, @admin_role);

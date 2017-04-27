43a0a8d547d7e7fa0aca5594a992351401bbe806
engsyst <eng.syst@gmail.com>
2017-04-27 10:55:13 +0300

    Same as previous

--- src/main/java/ua/nure/indplan/controller/UserController.java
+++ src/main/java/ua/nure/indplan/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;

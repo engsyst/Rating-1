c80fc0ea1c81a043c320fd3cf436c863609c5519
engsyst <eng.syst@gmail.com>
2018-10-31 16:25:10 +0200

    Test for Excel Parser

--- src/test/java/ua/nure/indplan/RatingApplicationTests.java
+++ src/test/java/ua/nure/indplan/RatingApplicationTests.java
@@ -1,16 +1,20 @@
-package net.ua;
+package ua.nure.indplan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import ua.nure.indplan.RatingApplication;
+import ua.nure.indplan.excel.parser.ExcelParserTest;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RatingApplication.class)
+@SpringApplicationConfiguration(classes = { 
+//		RatingApplication.class, 
+		ExcelParserTest.class, 
+	})
 @WebAppConfiguration
 public class RatingApplicationTests {
 
---
+++ src/test/java/ua/nure/indplan/excel/parser/ExcelParserTest.java
@@ -0,0 +1,28 @@
+package ua.nure.indplan.excel.parser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ua.nure.indplan.excel.parser.entity.ParsedExcelDisciplines;
+
+public class ExcelParserTest {
+
+	final String fName = "test-data/NAgruzka_ Міщеряков Ю. В. (Професор)_old.xls";
+	InputStream in;
+
+	@Before
+	public void setUp() throws Exception {
+		in = new FileInputStream(fName);
+	}
+
+	@Test
+	public void testGetParsedExel() throws IOException {
+		ExcelParser.getParsedExel(in);
+	}
+
+}
